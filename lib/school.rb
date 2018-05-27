2e5a1f969629731bd2bdd5390ce86073dd23110f
Janice Darikho <janicedarikho@gmail.com>
2018-05-27 02:09:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,12 +3,9 @@ class School
 
   def initialize(school_name)
     @school_name = school_name
-  end
-  
-  def roster
     @roster = Hash.new
   end
-  
+
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade
