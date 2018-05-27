914789f6f1ec798505f35eab6cf2fe847f4e10e0
Janice Darikho <janicedarikho@gmail.com>
2018-05-27 02:02:50 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,19 @@
 class School
-  attr_accessor :roster
-  
-  RECORDS = {}
+  attr_reader :roster
 
   def initialize(school_name)
     @school_name = school_name
   end
   
+  def roster
+    @roster = Hash.new
+  end
+  
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade
-    if RECORDS.keys.include?(grade)
-      RECORDS[grade] << student_name
+    if @roster.keys.include?(grade)
+      @roster[grade] << student_name
     else
       RECORDS[grade] = []
       RECORDS[grade] << student_name
