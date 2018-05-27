a38021586ba741d48f8e95b880e6519c94f617a0
Janice Darikho <janicedarikho@gmail.com>
2018-05-27 01:19:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  def initialize(school_name)
+    @school_name = school_name
+  end
+  
+  def roster 
+    @records = {}
+  end
+  
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    @records[grade] = []
+    @records = {
+      grade => [student_name]
+    }
+  end
+
+end
